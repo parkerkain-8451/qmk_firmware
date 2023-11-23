8ecc90ebc11ee05e0a3b4cd78be49ae0ab8a9531
Parker Kain <parker.kain@8451.com>
2023-11-23 09:11:23 -0500

    New Layer

--- keyboards/crkbd/keymaps/parkers_keymap/keymap.c
+++ keyboards/crkbd/keymaps/parkers_keymap/keymap.c
@@ -96,11 +96,23 @@ XXXXXXX,MT(MOD_LGUI,KC_A),MT(MOD_LALT,KC_R),MT(MOD_LCTL,KC_S),MT(MOD_LSFT, KC_T)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      XXXXXXX, XXXXXXX,XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,                     RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI,  RGB_VAI, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, KC_LSFT, XXXXXXX,                     XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, KC_LSFT, XXXXXXX,                     DF(7), KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    RGB_M_P, RGB_M_B,  RGB_M_R, RGB_M_G, RGB_M_K, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS, XXXXXXX, XXXXXXX,     KC_MSTP, KC_MPLY, KC_MUTE
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [7] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,     KC_U,   KC_I,     KC_O,    KC_P,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,     KC_J,   KC_J,     KC_K,    KC_L,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,     KC_M,   KC_COMMA, KC_DOT, KC_SLASH, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TAB, KC_SPACE, KC_LSFT,     XXXXXXX, DF(0), XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
